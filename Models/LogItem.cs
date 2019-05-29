6f9a22b75d115ea35b246be6981d0a1b868b367a
bsha <ben.sha@advantagecs.com>
2019-05-29 09:59:12 -0400

    Check text length before calling Substring method
    Avoid ArgumentOutOfRangeException if text length is less than 75 characters.

--- Models/LogItem.cs
+++ Models/LogItem.cs
@@ -5,7 +5,7 @@
         public string Text { get; set; }
         public string Date { get; set; }
         public string Preview {
-            get { return Text.Substring(0, 75) + "..."; }
+            get { return Text.Length > 75 ? Text.Substring(0, 75) + "..." : Text; }
         }
     }
 }
